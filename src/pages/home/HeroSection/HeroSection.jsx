bb83fe2f3a6d26d7cdbfba67087ff9d8c278e193
Younus Shamim <younusshamim.bd@gmail.com>
2024-01-24 00:50:11 +0600

    design home page hero section

---
+++ src/pages/home/HeroSection/HeroSection.jsx
@@ -0,0 +1,33 @@
+import { Box, Button, Heading, Image, Stack } from "@chakra-ui/react";
+import heroBg from "../../../assets/HeroBg.jpg";
+
+const HeroSection = () => {
+  return (
+    <Stack
+      justify="space-between"
+      alignItems="center"
+      px="80px"
+      bg="#000"
+      position="relative"
+      pb="96px"
+    >
+      <Image src={heroBg} alt="" h="640px" w="full" borderRadius="8px" />
+
+      <Box position="absolute" bottom="140px" textAlign="center" w="full">
+        <Heading mb="4" color="#fff" fontWeight="500" fontSize="48px">
+          Not sure where to go? Perfect.
+        </Heading>
+        <Button
+          w="fit-content"
+          color="#671296"
+          borderRadius="100px"
+          p="16px 24px"
+        >
+          I’m flexible
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
+
+export default HeroSection;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import { Stack } from "@chakra-ui/react";
 import Navbar from "./Navbar/Navbar";
 import SearchBar from "./SearchBar/SearchBar";
+import HeroSection from "./HeroSection/HeroSection";
 
 const Home = () => {
   return (
     <Stack gap="0">
       <Navbar />
       <SearchBar />
+      <HeroSection />
     </Stack>
   );
 };
